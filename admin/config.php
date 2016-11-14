940c72652bab302435cd82fd1e0da16009c46c0e
DatsivStepan <datsivStepan@gmail.com>
2016-11-14 11:04:57 +0200

    first commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/adastra/admin/');
+define('HTTP_CATALOG', 'http://localhost/adastra/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/adastra/admin/');
+define('HTTPS_CATALOG', 'http://localhost/adastra/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp/www/adastra/admin/');
+define('DIR_SYSTEM', 'C:/wamp/www/adastra/system/');
+define('DIR_LANGUAGE', 'C:/wamp/www/adastra/admin/language/');
+define('DIR_TEMPLATE', 'C:/wamp/www/adastra/admin/view/template/');
+define('DIR_CONFIG', 'C:/wamp/www/adastra/system/config/');
+define('DIR_IMAGE', 'C:/wamp/www/adastra/image/');
+define('DIR_CACHE', 'C:/wamp/www/adastra/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp/www/adastra/system/storage/download/');
+define('DIR_LOGS', 'C:/wamp/www/adastra/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/wamp/www/adastra/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/wamp/www/adastra/system/storage/upload/');
+define('DIR_CATALOG', 'C:/wamp/www/adastra/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'adastra');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
